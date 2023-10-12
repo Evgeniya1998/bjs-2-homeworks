505206d7e1c4c0c3eca03c23a15ecf4e237c07b8
Evgesha <Slav.novikova2014@gmail.com>
2023-10-12 12:54:48 +0700

    Задание 1 выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,36 @@
 function getArrayParams(...arr) {
+	let min = getArrayParams[0];
+	let max = getArrayParams[0];
+	let sum;
+	getArrayParams(5, 1, 0, 56, 43, 7, 9);
+	for (let i = 0; i < arr.length; i++) {
+		if ((getArrayParams[i]) < min) {
+			min = getArrayParams[i];
+		}
+		console.log(arr[min]);
 
-  return { min: min, max: max, avg: avg };
+		for (let i = 0; i < arr.length; i++) {
+			if ((getArrayParams[i]) >= max) {
+				max = getArrayParams[i];
+			}
+			console.log(arr[max]);
+
+			for (let i = 0; i < arr.length; i++) {
+				sum = +getArrayParams[i];
+			}
+			console.log(arr[max]);
+			return {
+				min: min,
+				max: max,
+				avg: avg
+			};
+
+		}
+	}
+
 }
+
+
 
 function summElementsWorker(...arr) {
 
