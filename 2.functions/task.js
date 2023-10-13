163558c588fe4421ab9e3d6f45c6388d0dbcda9c
Evgesha <Slav.novikova2014@gmail.com>
2023-10-13 15:30:12 +0700

    Задание 1,2 выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -27,30 +27,85 @@ console.log(getArrayParams(5));
 
 
 function summElementsWorker(...arr) {
-  for (let i = 0; i < arr.length; i++) {
-		sum = +arr[i];
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum +=arr[i];
 	}
-	const avg = Number((sum / arr.length).toFixed(2));
-
 
 	return {
 		sum: sum
-	};
-
-
+	}
 }
+console.log(summElementsWorker()); // 0
+console.log(summElementsWorker(10, 10, 11, 20, 10)); // 61
 
 function differenceMaxMinWorker(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let difference;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
 
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		difference = max - min;
+	}
+
+	return {
+		difference: difference,
+		max: max,
+		min: min
+	}
 }
+console.log(differenceMaxMinWorker()); // 0
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
 
 function differenceEvenOddWorker(...arr) {
+	sumEvenElement = 0;
+	sumOddElement = 0;
+	if (arr.length === 0) {
+		return 0;
+	}
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i]
 
+		}
+		difference = sumEvenElement - sumOddElement;
+	}
+	return {
+		sumEvenElement: sumEvenElement,
+		sumOddElement: sumOddElement,
+		difference: difference
+
+	}
 }
+console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17)); // 266 - 213 => 53
+console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // 114 - 383 => -269
 
 function averageEvenElementsWorker(...arr) {
-
+	sumEvenElement = 0;
+	countEvenElement = 0;
+	if (arr.length === 0) {
+		return 0;
+	}
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++
+		}
+	}
+	if (countEvenElement === 0)
+		return 0;
+	return (sumEvenElement / countEvenElement);
 }
+console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
+console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
 
 function makeWork (arrOfArr, func) {
 
