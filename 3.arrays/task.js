1d8d097c0bf3b4ab1b856f93882582fbd5eb78ce
Evgesha <Slav.novikova2014@gmail.com>
2023-10-17 11:06:27 +0700

    Задание 1 выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+	var isEqual = arr1.length === arr2.length &&
+		arr1.every((value, index) => value === arr2[index])
+
+	return isEqual;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
   
 }
+
+
