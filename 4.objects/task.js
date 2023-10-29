a5573a0c7fae74052132e66bc2a444e4d2dd24fd
Evgesha <Slav.novikova2014@gmail.com>
2023-10-29 14:11:09 +0700

    Выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,21 +1,30 @@
 function Student(name, gender, age) {
-
-    
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks) {
+		this.marks.push(...marksToAdd);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks == []) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
