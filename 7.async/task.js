dc7800afed19ea0b1f45656952cc9e8a2331674e
Evgesha <Slav.novikova2014@gmail.com>
2023-11-12 14:43:53 +0700

    Задания выполнены

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,71 +1,65 @@
 class AlarmClock {
-    constructor () {
-        this.alarmCollection = [];
-        this.intervalId = null;
-    }
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
 
-    addClock(time, callback) {
+	addClock(time, callback) {
 
-        if (!time || !callback) {
-            throw new Error('Отсутствуют обязательные аргументы')
-        }
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы')
+		}
 
-        if (this.alarmCollection.find(alarm => alarm.time === time)) {
-            console.warn('Уже присутствует звонок на это же время');
-        }
-        
-        this.alarmCollection.push({
-            callback: callback,
-            time: time,
-            canCall: true
-        });          
-    }
+		if (this.alarmCollection.find(alarm => alarm.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
 
-    removeClock(time) {
+		this.alarmCollection.push({
+			callback: callback,
+			time: time,
+			canCall: true
+		});
+	}
 
+	removeClock(time) {
 
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
 
-        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
-    }
+	getCurrentFormattedTime() {
 
-    getCurrentFormattedTime() {
+		return new Date().toLocaleTimeString("ru-Ru", {
+			hour: "2-digit",
+			minute: "2-digit",
+		})
+	}
 
+	start() {
+		if (this.intervalId) {
+			return;
+		}
+		this.intervalId = setInterval(() => {
+			const currentTime = this.getCurrentFormattedTime();
+			this.alarmCollection.forEach(element => {
+				if (element.time === currentTime && element.canCall) {
+					element.callback();
+					element.canCall = false;
+				}
+			})
 
-        
-      return new Date().toLocaleTimeString("ru-Ru", {
-      hour: "2-digit",
-      minute: "2-digit",
-      })
-    }
+		}, 1000)
+	}
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
 
-     
+	resetAllCalls() {
+		this.alarmCollection.forEach(element => element.canCall = true);
+	}
 
-    start() {
-        if (this.intervalId) {
-            return;
-        } 
-        this.intervalId = setInterval(() => {
-            const currentTime = this.getCurrentFormattedTime();
-            this.alarmCollection.forEach(element => {
-                if (element.time === currentTime && element.canCall) {
-                    element.callback();
-                    element.canCall = false;
-                }
-            })
-                
-        }, 1000) 
-    } 
-    stop() {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-    }
-
-    resetAllCalls() {
-        this.alarmCollection.forEach(element => element.canCall = true);
-    }
-
-    clearAlarms() {
-        this.stop();
-        this.alarmCollection = [];
-    }
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
 }
